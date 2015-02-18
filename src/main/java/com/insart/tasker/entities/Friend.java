c60f89ce510bbb56f332586d3fead7229fb0f524
Nastya <asya.gorobec@gmail.com>
2015-02-18 21:30:11 +0200

    add Friend  class

--- src/main/java/com/insart/tasker/entities/Friend.java
+++ src/main/java/com/insart/tasker/entities/Friend.java
@@ -19,6 +19,22 @@ public class Friend {
     @Column(name = "flag")
     private Boolean flag;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
+    }
+
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "friend")
     private Set<User> users;
 
